72754dd86f3417c917b2c638c1d4440e59482ea7
xuyzgitee <404321218@qq.com>
2020-12-06 14:37:06 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+// 加载全局样式文件
+import '../src/styles/index.less';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
